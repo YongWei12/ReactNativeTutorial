1541e92446bc4aa5894d4ea0f4655dc4bc57609e
Yong Wei <yongwei996@gmail.com>
2020-05-28 13:16:25 +0800

    added an image to the app

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import BodyText from '../components/BodyText'
+import TitleText from '../components/TitleText'
 
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Game is Over</Text>
-            <Text>Number of rounds: {props.roundNumber}</Text>
-            <Text>Number was: {props.userNumber}</Text>
-            <Button title="New Game" onPress={props.onRestart}/>
+            <TitleText>The Game is Over</TitleText>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image}
+                    source={require('../assets/success.png')}
+                    // source={{ uri: 'https://www.scitecheuropa.eu/wp-content/uploads/2020/01/Mount-Everest-696x392.jpg' }}
+                    resizeMode="stretch" />
+            </View>
+            <BodyText>Number of rounds: {props.roundNumber}</BodyText>
+            <BodyText>Number was: {props.userNumber}</BodyText>
+            <Button title="New Game" onPress={props.onRestart} />
         </View>
     )
 }
@@ -18,6 +26,20 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+
+    imageContainer: {
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        width: 300,
+        height: 300,
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    image: {
+        width: '100%',
+        height: '100%'
     }
 })
 
