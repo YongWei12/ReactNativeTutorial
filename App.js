27713e0f5b35aa523b26dc3cb3abf0f039344775
Yong Wei <yongwei996@gmail.com>
2020-05-28 12:08:30 +0800

    working font loading, need to use async await since it is asyncronous

--- App.js
+++ App.js
@@ -1,36 +1,57 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo'
+
 
 import Header from './components/Header'
 import StartGameScreen from './screens/StartGameScreen';
-import GameScreen from './screens/GameScreen'
+import GameScreen from './screens/GameScreen';
 import GameOverScreen from './screens/GameOverScreen';
 
+//function no need to be re created in every re render
+const fetchFonts = async () => {
+  return (await Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  })
+  );
+};
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0)
+  const [dataLoaded, setDataLoaded] = useState(false);
 
-  const configureNewGame = ()=>{
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(err) => console.log(err)} />
+    )
+  }
+
+  const configureNewGame = () => {
     setGuessRounds(0);
     setUserNumber(null);
   }
 
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
-    
+
   }
 
-  const gameOverHandler= numOfRounds => {
+  const gameOverHandler = numOfRounds => {
     setGuessRounds(numOfRounds);
   }
 
   let content = <StartGameScreen onStartGame={startGameHandler} />
 
-  if (userNumber && guessRounds<=0) {
-    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler}/>
-  }else if(guessRounds >0 ){
-    content= <GameOverScreen roundNumber={guessRounds}  userNumber={userNumber} onRestart={configureNewGame}/>
+  if (userNumber && guessRounds <= 0) {
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+  } else if (guessRounds > 0) {
+    content = <GameOverScreen roundNumber={guessRounds} userNumber={userNumber} onRestart={configureNewGame} />
   }
   return (
     <View style={styles.screen}>
